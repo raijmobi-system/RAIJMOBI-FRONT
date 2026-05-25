6d6a5299d9fa56854f0859dabde8d13d654abbbc
Pablo Murilo <pablo.murilo10@hotmail.com>
2026-05-25 13:47:24 -0300

    removendo componentes de teste

--- components/atoms/Box.tsx
+++
@@ -1,8 +0,0 @@
-import { styled } from '../../styled-system/jsx';
-
-/**
- * Box é o contêiner genérico base.
- * Ele aceita qualquer token do seu tema (bg, p, m, etc) 
- * e pode se transformar em qualquer tag HTML usando a prop 'as'.
- */
-export const Box = styled('div');
